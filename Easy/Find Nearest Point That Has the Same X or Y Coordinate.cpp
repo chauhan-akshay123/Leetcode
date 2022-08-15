1682b15500b8fcadca2138b5abe7134293ebcbe4
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-08-15 12:56:00 +0530

    Update Find Nearest Point That Has the Same X or Y Coordinate.cpp

--- Easy/Find Nearest Point That Has the Same X or Y Coordinate.cpp
+++ Easy/Find Nearest Point That Has the Same X or Y Coordinate.cpp
@@ -2,21 +2,21 @@
 ---LINK--- https://leetcode.com/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
 */
 
- int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
-        int min = INT_MAX,index=-1;
-        for(int i=0; i<points.size(); i++){
-            int x1 = points[i][0];
-            int y1 = points[i][1];
-            if((x1!=x) && (y1!=y)){
-                continue;
-            }
-            int dis = abs(x-x1)+abs(y-y1);
-            if(dis<min){
-                min=dis;
-                index=i;
-            }
-        }
-        return index;
+    int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+             int mn=INT_MAX, res=-1;
+          for(int i=0;i<points.size();i++){
+      
+      if(points[i][0]== x || points[i][1]==y){
+           
+          int md= abs(points[i][0] - x) + abs(points[i][1] - y);
+          
+          if(md < mn){
+              mn = md;
+              res= i;
+          }
+      }
+    }
+   return res; 
     }
 
 
