8432075ffa1f6f4d7f30e3a7b7f661e74aae0915
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-08-15 21:19:18 +0530

    Create Shuffle the Array.cpp

---
+++ Easy/Shuffle the Array.cpp
@@ -0,0 +1,12 @@
+/*
+---LINK--- https://leetcode.com/problems/shuffle-the-array/
+*/
+  vector<int> shuffle(vector<int>& nums, int n) {
+      vector<int>ans;
+        for(int i=0 ; i<n ; ++i)
+        {
+           ans.push_back(nums[i]);
+           ans.push_back(nums[i+n]);
+        }
+        return ans;
+    }
