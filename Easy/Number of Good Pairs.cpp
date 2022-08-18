0fb322c4c400495ac5139148c05daf63dc6c4d03
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-08-18 10:26:43 +0530

    Create Number of Good Pairs.cpp

---
+++ Easy/Number of Good Pairs.cpp
@@ -0,0 +1,14 @@
+/*
+---LINK--- https://leetcode.com/problems/number-of-good-pairs/
+*/
+   int numIdenticalPairs(vector<int>& nums) {
+        int count=0;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i]==nums[j]){
+                  count++;  
+                }
+            }
+        }
+        return count;
+    }
