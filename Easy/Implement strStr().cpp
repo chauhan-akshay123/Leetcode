78fc05dfd257474714e098c79880bc8afaf32638
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-08-25 12:49:11 +0530

    Update Implement strStr().cpp

--- Easy/Implement strStr().cpp
+++ Easy/Implement strStr().cpp
@@ -13,3 +13,14 @@
         }
         return -1;
     }
+// Approach-2
+  int strStr(string haystack, string needle) {
+        for(int i=0; i<haystack.length(); i++){
+            int j=0;
+            for(; haystack[i+j]==needle[j] && j<needle.length(); j++);
+             if(j==needle.length()){
+                 return i;
+             }
+            }
+        return -1;
+        }
