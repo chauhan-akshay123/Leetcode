a8b6efebfd18a263a34dc652e0bf7e5fb949d4bb
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-08-25 10:42:16 +0530

    Create Monotonic Array.cpp

---
+++ Easy/Monotonic Array.cpp
@@ -0,0 +1,33 @@
+/*
+---LINK--- https://leetcode.com/problems/monotonic-array/
+*/
+    bool isMonotonic(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0){
+          return true;
+      }
+      int i;
+      for(i=0; i<n-1; i++){
+          if(nums[i]<=nums[i+1]){
+              continue;
+          }
+          else{
+              break;
+          }
+      }  
+      if(i==(n-1)){
+          return true;
+      }
+      for(i=0; i<n-1; i++){
+          if(nums[i]>=nums[i+1]){
+             continue; 
+          }
+          else{
+              break;
+          }
+      }  
+      if(i==(n-1)){
+          return true;
+      }  
+      return false;
+    }
