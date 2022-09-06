126465614b895b7f4daed9886c622ae06a707bf8
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-09-06 18:55:51 +0530

    Create Reverse Integer.cpp

---
+++ Easy/Reverse Integer.cpp
@@ -0,0 +1,18 @@
+/*
+---LINK--- https://leetcode.com/problems/reverse-integer/
+*/
+   int reverse(int x) {
+    int rev=0;
+        while(x!=0){
+        int digit=x%10;
+        x=x/10;
+        if(rev>INT_MAX/10 || (rev==INT_MAX/10 && digit>INT_MAX%10)){
+            return 0;
+        }
+        if(rev<INT_MIN/10 || (rev==INT_MIN/10 && digit<INT_MIN%10)){
+            return 0;
+        }   
+        rev=rev*10+digit;
+        }
+        return rev;   
+    }
