e6799369f9d89248f06c96bc9ae774b5d7dd8c56
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-09-13 21:35:48 +0530

    Create Palindrome Number.cpp

---
+++ Easy/Palindrome Number.cpp
@@ -0,0 +1,15 @@
+/*
+---LINK--- https://leetcode.com/problems/palindrome-number/
+*/
+   bool isPalindrome(int x) {
+        long long int temp = x;
+        long long int rev=0;
+        while(x>0)
+        {
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        if(rev==temp)
+            return true;
+        return false;
+    }
