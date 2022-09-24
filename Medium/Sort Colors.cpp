1bb9cd8b9adab5b26017bd9f6dc1c188cfa42343
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-09-24 22:01:29 +0530

    Create Sort Colors.cpp

---
+++ Medium/Sort Colors.cpp
@@ -0,0 +1,21 @@
+/*
+---LINK--- https://leetcode.com/problems/sort-colors/
+*/
+ void sortColors(vector<int>& nums) {
+        int p0=0,p=0,p2=nums.size()-1;
+    while(p<=p2){
+        if(nums[p]==0){
+        nums[p]=nums[p0];
+        nums[p0]=0;
+        p++;
+        p0++;    
+        }
+        else if(nums[p]==2){
+          nums[p]=nums[p2];
+          nums[p2]=2;
+          p2--;  
+        }else{
+            p++;
+        }
+    }
+    }
