82879ec126c1bee5fa3207ecf02e4584c81064f6
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-09-30 22:54:01 +0530

    Create Rearrange Array Elements by Sign.cpp

---
+++ Medium/Rearrange Array Elements by Sign.cpp
@@ -0,0 +1,27 @@
+/*
+---LINK--- https://leetcode.com/problems/rearrange-array-elements-by-sign/
+*/
+
+    int n = nums.size();
+        vector<int> res;
+        int pos = 0,neg = 0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                if(nums[pos]>0)
+                    res.push_back(nums[pos++]);
+                else{
+                    while(nums[pos]<0) pos++;
+                    res.push_back(nums[pos++]);
+                }
+            }
+            else{
+                if(nums[neg]<0)
+                    res.push_back(nums[neg++]);
+                else{
+                    while(nums[neg]>0) neg++;
+                    res.push_back(nums[neg++]);
+                }
+            }
+        }
+        return res;
+    }
