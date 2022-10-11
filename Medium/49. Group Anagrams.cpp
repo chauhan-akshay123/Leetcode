eb281aa6059cab60bec85ac16374994f6ae386f1
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-10-11 20:50:52 +0530

    Create 49. Group Anagrams.cpp

---
+++ Medium/49. Group Anagrams.cpp
@@ -0,0 +1,20 @@
+/*
+---LINK--- https://leetcode.com/problems/group-anagrams/
+*/
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       vector<vector<string>> ans;
+       unordered_map <string,vector<string>> umap;
+        
+       for(auto x:strs){
+          string temp = x;
+          sort(x.begin(),x.end());
+          umap[x].push_back(temp); 
+       } 
+       
+       for(auto x: umap){
+          vector<string> temp = x.second; 
+          ans.push_back(temp); 
+       } 
+       return ans; 
+    }
