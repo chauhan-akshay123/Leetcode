edc85a2680bcc9a0fd967c288bafdf4a64b21cd5
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-10-11 20:57:16 +0530

    Create 242. Valid Anagram.cpp

---
+++ Easy/242. Valid Anagram.cpp
@@ -0,0 +1,12 @@
+/*
+---LINK--- https://leetcode.com/problems/valid-anagram/
+*/
+
+    bool isAnagram(string s, string t) {
+         sort(s.begin(),s.end());
+         sort(t.begin(),t.end());
+         if(s==t){
+            return true; 
+         }
+        return false;
+    }
