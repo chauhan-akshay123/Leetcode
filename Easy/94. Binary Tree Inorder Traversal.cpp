f1ebd61faf38acfb558fc0e1df9b3cc80b971325
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-10-12 10:57:36 +0530

    Create 94. Binary Tree Inorder Traversal.cpp

---
+++ Easy/94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,17 @@
+/*
+--- LINK --- https://leetcode.com/problems/binary-tree-inorder-traversal/
+*/
+
+   void inorder(TreeNode* root, vector<int> &ans){
+         if(root!=NULL){
+            inorder(root->left,ans);
+            ans.push_back(root->val);
+            inorder(root->right,ans); 
+         }
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+          vector<int> ans;
+          inorder(root,ans);
+          return ans;        
+    }
