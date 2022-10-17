6ebe58cc3f11b66f0d6e11f61e861c9d57e6fcd8
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-10-17 18:52:18 +0530

    Create 572. Subtree of Another Tree.cpp

---
+++ Easy/572. Subtree of Another Tree.cpp
@@ -0,0 +1,43 @@
+/*
+---Leetcode LINK--- https://leetcode.com/problems/subtree-of-another-tree/
+*/
+
+class Solution {
+public:
+    int ans = false;
+    
+    bool match(TreeNode* root,TreeNode* subRoot){
+        if(root!=NULL && subRoot!=NULL){
+           bool a = match(root->left,subRoot->left);
+           bool b = match(root->right,subRoot->right);
+           if((root->val==subRoot->val) && a && b){
+              return true; 
+           }
+           else{
+               return false;
+           } 
+        }
+        else if(root==NULL && subRoot==NULL){
+           return true; 
+        }
+        else{
+            return false;
+        }
+    }
+    
+    void inorder(TreeNode* root,TreeNode* subRoot){
+        if(root!=NULL){
+           inorder(root->left,subRoot);
+           bool x = match(root,subRoot);
+           if(x){
+              ans = true; 
+           } 
+           inorder(root->right,subRoot); 
+        }
+    }
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        inorder(root,subRoot);
+        return ans;
+    }
+};
